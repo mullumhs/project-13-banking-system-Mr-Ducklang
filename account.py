8977847434a5c6d66c9d905140711c6f6c301a4f
Mr-Ducklang <136151387+Mr-Ducklang@users.noreply.github.com>
2024-06-14 00:40:05 +0000

    Account Class done BankManager In progress

--- account.py
+++ account.py
@@ -4,4 +4,51 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#Account Class
+class Account:
+    def __init__(self, name, balance, password, ):
+        self.set_name(name)
+        self.set_balance(balance)
+        self.set_password(password)
+        
+        
+#Getters
+    def get_name(self):
+        return self._name
 
+    def get_balance(self):
+        return f"${self._balance:.2f}"
+
+    def get_password(self):
+        return self._password
+
+    #Setters
+    def set_name(self, new_name):
+        if not isinstance(new_name, str):
+            raise ValueError("Error! New name is not a string!")
+        self._name = new_name
+        
+    def set_balance(self, new_balance):
+        if new_balance is str:
+            raise ValueError("Error! New Balance must be a number!")
+        self._balance = new_balance
+   
+    def set_password(self, new_password):
+        user_input = input("What is your current password?")
+        if user_input is self._password:
+            self._password = new_password
+        else:
+            ("Incorrect Password!")
+
+
+    #Methods
+    
+    def deposit(self, deposit):
+        self.get_balance()
+        self._balance = self._balance + deposit
+        
+    def withdraw(self, withdrawal):
+        self.get_balance()
+        if withdrawal > self.balance:
+            raise ValueError(f"Error! Cannot withdraw more than account balance!, your account balance is {self._balance}")
+        self._balance = self._balance - withdrawal
--- bank.py
+++ bank.py
@@ -4,3 +4,38 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#import Account Class
+from account import Account
+
+#BankManager Class
+class BankManager:
+    def __init__(self):
+        self.accounts = []
+        
+    #adding accounts
+    def add_account(self, account):
+        #check for duplicate
+        for existing_account in self.accounts:
+            if existing_account.get_name() == self.accounts():
+                print("Error, this account name already exists, account not added")
+                return False
+        #making account
+        self.accounts.append(account)
+        return True
+            
+    #removing account
+    def remove_account(self, name):
+        for i, existing_account in enumerate(self.accounts):
+            if existing_account.get_name() == name:
+                del self.accounts[i]
+                return True
+        print("Error! Account not found")
+        return False
+    
+    #managing deposits, withdrawals and transfers
+    def manage_transactions():
+        print()
+        
+    #manage account balance, name, passwords
+    def manage_accounts():
+        print()
