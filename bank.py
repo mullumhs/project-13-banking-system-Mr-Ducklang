a3e2c1c61c091f9f1bb525f4281733462cd4f7f8
Mr-Ducklang <136151387+Mr-Ducklang@users.noreply.github.com>
2024-06-16 23:48:40 +0000

    BankManager almost done

--- bank.py
+++ bank.py
@@ -7,11 +7,20 @@
 #import Account Class
 from account import Account
 
+
+    
+    
 #BankManager Class
 class BankManager:
     def __init__(self):
         self.accounts = []
-        
+           
+    def acc_check(self, name):
+        for existing_account in self.accounts:
+            if existing_account.get_name() == name:
+                return existing_account
+        return False 
+   
     #adding accounts
     def add_account(self, account):
         #check for duplicate
@@ -33,9 +42,39 @@ class BankManager:
         return False
     
     #managing deposits, withdrawals and transfers
-    def manage_transactions():
+    def deposits(self, name, deposit_amount):
+        existing_account = self.acc_check(name)
+        if existing_account is not False:
+            existing_account.deposit(deposit_amount)
+            return True
+        print("Error! Account not found")
+        return False
+    
+    def withdrawals(self, name, withdraw_amount):
+        existing_account = self.acc_check(name)
+        if existing_account is not False:
+            existing_account.withdraw(withdraw_amount)
+            return True
+        print("Error! Account not found")
+        return False
+    
+    def transfers(self, name1, name2, transfer_amount):
+        existing_account = self.acc_check(name1)
+        if existing_account is not False:
+            existing_account.withdraw(transfer_amount)
+        existing_account2 = self.acc_check(name2)
+        if existing_account2 is not False:
+            existing_account2.deposit(transfer_amount)
+            return True
+        print("Error! Account not found")
+        return False
+            
+    #manage account balance, name, passwords
+    def manage_acc_name():
         print()
         
-    #manage account balance, name, passwords
-    def manage_accounts():
+    def manage_acc_password():
+        print()
+        
+    def manage_acc_name():
         print()
